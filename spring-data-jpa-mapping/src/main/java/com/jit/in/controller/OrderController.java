ce2dab71875a73464a442219866c9388533eefb3
Jyoti-Mishra-123 <mishrajyoto3693@gmail.com>
2021-08-04 22:48:36 +0530

    controller changes

--- spring-data-jpa-mapping/src/main/java/com/jit/in/controller/OrderController.java
+++ spring-data-jpa-mapping/src/main/java/com/jit/in/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.jit.in.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,8 +26,8 @@ public class OrderController {
 	private ProductRepo prepo;
 	
 	@PostMapping(value = "/placeOrder",consumes= {("application/json")})
-	public Customer placeOrder(@RequestBody OrderRequest od) {
-		return crepo.save(od.getCustomer());
+	public ResponseEntity<Customer> placeOrder(@RequestBody OrderRequest od) {
+		return new ResponseEntity<>(crepo.save(od.getCustomer()),HttpStatus.CREATED);
 		
 	}
 	
